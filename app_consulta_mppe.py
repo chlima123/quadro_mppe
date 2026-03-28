5304659d7cc0697b9b16d290cdb0448dc55a0e94
chlima123 <164437261+chlima123@users.noreply.github.com>
2026-03-28 17:04:48 -0300

    Refactor app_consulta_mppe.py for data loading

--- app_consulta_mppe.py
+++ app_consulta_mppe.py
@@ -5,7 +5,7 @@ BASES_DADOS = {
     "1a Entrancia": "https://portal.mppe.mp.br/documents/d/guest/quadro-geral-membros-mppe-oficial-1-entrancia?download=true",
     "2a Entrancia": "https://portal.mppe.mp.br/documents/d/guest/quadro-geral-membros-mppe-oficial-2-entrancia?download=true",
     "3a Entrancia": "https://portal.mppe.mp.br/documents/d/guest/quadro-geral-membros-mppe-oficial-3-entrancia?download=true",
-    "2a Instancia": "https://portal.mppe.mp.br/documents/d/guest/quadro-geral-membros-mppe-oficial-2-instancia?download=true",
+    "2a Instancia": "https://portal.mppe.mp.br/documents/d/guest/quadro-geral-membros-mppe-oficial-2-instancia-1?download=true",
 }
 COLUNAS_FILTRO = ["Circunscricao", "Municipio", "Atuacao", "Atribuicao"]
 COLUNAS_SAIDA = ["Circunscricao", "Municipio", "Cargo", "Atribuicao", "Atuacao", "Promotor de Justica"]
